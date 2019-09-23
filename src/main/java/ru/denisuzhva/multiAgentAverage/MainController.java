4325c05bd2495d5ee59274f1f37cdf9e5ff79883
mortarsynth <epirtochol@gmail.com>
2019-09-23 21:01:14 +0300

    estabilished communications

--- src/main/java/ru/denisuzhva/multiAgentAverage/MainController.java
+++ src/main/java/ru/denisuzhva/multiAgentAverage/MainController.java
@@ -1,14 +1,28 @@
 package main.java.ru.denisuzhva.multiAgentAverage;
 
+import com.sun.tools.javac.Main;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 
+import java.util.HashMap;
+
 
 class MainController {
     private static final int numberOfAgents = 5;
+    private static HashMap<Integer, Integer[]> agentConnectivity;
+
+    MainController() {
+        agentConnectivity = new HashMap<Integer, Integer[]>();
+        agentConnectivity.put(0, new Integer[] {1, 2, 3, 4, 5});
+        agentConnectivity.put(1, new Integer[]{0, 2, 3, 4, 5});
+        agentConnectivity.put(2, new Integer[]{0, 1, 3, 4, 5});
+        agentConnectivity.put(3, new Integer[]{0, 1, 2, 4, 5});
+        agentConnectivity.put(4, new Integer[]{0, 1, 2, 3, 5});
+        agentConnectivity.put(5, new Integer[]{0, 1, 2, 3, 4});
+    }
 
     void initAgents() {
         Runtime rt = Runtime.instance();
--- src/main/java/ru/denisuzhva/multiAgentAverage/NumberAgent.java
+++ src/main/java/ru/denisuzhva/multiAgentAverage/NumberAgent.java
@@ -1,17 +1,81 @@
 package main.java.ru.denisuzhva.multiAgentAverage;
 
 import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+//import jade.core.behaviours.TickerBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.Random;
 
 
 public class NumberAgent extends Agent {
-    private String[] linkedAgents;
+    private Integer[] linkedAgents;
+    private String requestMessage = "Requesting the number...";
     private float number;
 
     @Override
     protected void setup() {
-        int id = Integer.parseInt(getAID().getLocalName());
-        System.out.println("Agent #" + id);
+        int agentId = Integer.parseInt(getAID().getLocalName());
+        Random rand = new Random();
+        number = rand.nextFloat();
+        System.out.println("Agent #" + agentId + " is ready; number guessed: " + number);
 
-        addBehaviour(new FindAverage(this, 6000));
+        // Hardcode the links
+        linkedAgents = new Integer[4];
+        for (int i = 0; i < 5; i++) {
+            if (i < agentId) {
+                linkedAgents[i] = i;
+            } else if (i > agentId) {
+                linkedAgents[i-1] = i;
+            }
+        }
+
+        addBehaviour(new WaitForNumberRequest());
+        addBehaviour(new SendNumberRequest());
+    }
+
+    @Override
+    protected void takeDown() {
+        int agentId = Integer.parseInt(getAID().getLocalName());
+        System.out.println("Agent #" + agentId + " is terminating");
+    }
+
+    private class SendNumberRequest extends OneShotBehaviour{
+        /*
+        SendNumberRequest(NumberAgent agent, Integer tick) {
+            super(agent, tick);
+        }
+         */
+
+        @Override
+        public void action() {
+            int agentId = Integer.parseInt(getAID().getLocalName());
+            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.setContent(requestMessage);
+            for (Integer linkedAgent : linkedAgents) {
+                msg.addReceiver(new AID(Integer.toString(linkedAgent), AID.ISLOCALNAME));
+                send(msg);
+            }
+        }
+    }
+
+    private class WaitForNumberRequest extends CyclicBehaviour {
+        @Override
+        public void action() {
+            ACLMessage msg = receive();
+            if (msg != null) {
+                if (msg.getContent().equals(requestMessage)) {
+                    int selfId = Integer.parseInt(getAID().getLocalName());
+                    int senderId = Integer.parseInt(msg.getSender().getLocalName());
+                    System.out.println("WAITING SERVICE: Agent #" + senderId +
+                            " requested the number from Agent #" + selfId +
+                            ". The number = " + number);
+                }
+            } else {
+                block();
+            }
+        }
     }
 }
