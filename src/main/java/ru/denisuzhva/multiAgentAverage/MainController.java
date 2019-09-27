862df2ba6ed2ca22d90c3602c725839a4066eb2f
mortarsynth <epirtochol@gmail.com>
2019-09-27 22:13:58 +0300

    finally done beta1

--- src/main/java/ru/denisuzhva/multiAgentAverage/MainController.java
+++ src/main/java/ru/denisuzhva/multiAgentAverage/MainController.java
@@ -8,16 +8,58 @@ import jade.core.Runtime;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 
-import java.util.HashMap;
+import java.util.*;
 
 
 class MainController {
-    private static final int numberOfAgents = 3;
+    private static int numberOfAgents = 3;
     private static HashMap<Integer, Integer[]> agentConnectivity;
 
     MainController() {
         agentConnectivity = new HashMap<Integer, Integer[]>();
-        // all nodes connected to each other
+        triangleGraphSix();
+    }
+
+    void initAgents() {
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.MAIN_HOST, "localhost");
+        p.setParameter(Profile.MAIN_PORT, "10098");
+        //p.setParameter(Profile.GUI, "true");
+        ContainerController cc = rt.createMainContainer(p);
+
+        try {
+            for (int i = 0; i < numberOfAgents; i++) {
+                AgentController agent = cc.createNewAgent(Integer.toString(i),
+                        "main.java.ru.denisuzhva.multiAgentAverage.NumberAgent",
+                        agentConnectivity.get(i));
+                agent.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Graph makers
+    private void triangleGraphSix() {
+        // 6 nodes only!
+        numberOfAgents = 6;
+        agentConnectivity.put(0, new Integer[] {1, 2});
+        agentConnectivity.put(1, new Integer[] {0, 2, 3, 4});
+        agentConnectivity.put(2, new Integer[] {0, 1, 4, 5});
+        agentConnectivity.put(3, new Integer[] {1, 4});
+        agentConnectivity.put(4, new Integer[] {1, 2, 3, 5});
+        agentConnectivity.put(5, new Integer[] {2, 4});
+    }
+
+    private void triangleGraphThree() {
+        numberOfAgents = 3;
+        agentConnectivity.put(0, new Integer[] {1, 2});
+        agentConnectivity.put(1, new Integer[] {0, 2});
+        agentConnectivity.put(2, new Integer[] {0, 1});
+    }
+
+    private void allConnected() {
         for (int agentIter = 0; agentIter < numberOfAgents; agentIter++) {
             Integer[] linkedAgents = new Integer[numberOfAgents-1];
             for (int i = 0; i < numberOfAgents; i++) {
@@ -29,37 +71,43 @@ class MainController {
             }
             agentConnectivity.put(agentIter, linkedAgents);
         }
-
-        /*
-        agentConnectivity.put(0, new Integer[] {1, 2, 3, 4, 5});
-        agentConnectivity.put(1, new Integer[]{0, 2, 3, 4, 5});
-        agentConnectivity.put(2, new Integer[]{0, 1, 3, 4, 5});
-        agentConnectivity.put(3, new Integer[]{0, 1, 2, 4, 5});
-        agentConnectivity.put(4, new Integer[]{0, 1, 2, 3, 5});
-        agentConnectivity.put(5, new Integer[]{0, 1, 2, 3, 4});
-
-         */
     }
 
-    void initAgents() {
-        Runtime rt = Runtime.instance();
-        Profile p = new ProfileImpl();
-        p.setParameter(Profile.MAIN_HOST, "localhost");
-        p.setParameter(Profile.MAIN_PORT, "10098");
-        p.setParameter(Profile.GUI, "true");
-        ContainerController cc = rt.createMainContainer(p);
-
-        try {
-            for (int i = 0; i < numberOfAgents; i++) {
-                System.out.println(i);
-                AgentController agent = cc.createNewAgent(Integer.toString(i),
-                        "main.java.ru.denisuzhva.multiAgentAverage.NumberAgent",
-                        agentConnectivity.get(i));
-                agent.start();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void straightConnection() {
+        numberOfAgents = 4;
+        Integer[] temp = new Integer[numberOfAgents-1];
+        for (int i = 1; i < numberOfAgents; i++) {
+            temp[i-1] = i;
         }
+        agentConnectivity.put(0, temp);
+        for (int agentIter = 1; agentIter < numberOfAgents; agentIter++) {
+            agentConnectivity.put(agentIter, new Integer[] {0});
+        }
+    }
+
+    private void diamondGraph() {
+        numberOfAgents = 4;
+        agentConnectivity.put(0, new Integer[] {1, 2});
+        agentConnectivity.put(1, new Integer[] {0, 3});
+        agentConnectivity.put(2, new Integer[] {0, 3});
+        agentConnectivity.put(3, new Integer[] {1, 2});
+    }
+
+    private void diamondGraphBar() {
+        numberOfAgents = 4;
+        agentConnectivity.put(0, new Integer[] {1, 2});
+        agentConnectivity.put(1, new Integer[] {0, 2, 3});
+        agentConnectivity.put(2, new Integer[] {0, 1, 3});
+        agentConnectivity.put(3, new Integer[] {1, 2});
+    }
+
+    private void lineGraph() {
+        numberOfAgents = 3;
+        agentConnectivity.put(0, new Integer[] {1});
+        for (int i = 1; i < numberOfAgents-1; i++) {
+            agentConnectivity.put(i, new Integer[] {i-1, i+1});
+        }
+        agentConnectivity.put(numberOfAgents-1, new Integer[] {numberOfAgents-2});
     }
 }
 
--- src/main/java/ru/denisuzhva/multiAgentAverage/NumberAgent.java
+++ src/main/java/ru/denisuzhva/multiAgentAverage/NumberAgent.java
@@ -2,32 +2,51 @@ package main.java.ru.denisuzhva.multiAgentAverage;
 
 import jade.core.Agent;
 import jade.core.AID;
-import jade.core.behaviours.CyclicBehaviour;
-//import jade.core.behaviours.TickerBehaviour;
-import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.*;
+//import jade.core.event.MessageAdapter;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+//import jade.tools.sniffer.Message;
+//import jade.tools.sniffer.Message;
+//import java.nio.channels.InterruptedByTimeoutException;
 
-import java.util.Random;
+import java.util.*;
+
 
 
 public class NumberAgent extends Agent {
     private Integer[] linkedAgents;
-    private String requestMessage = "Requesting the number...";
-    private float number;
+    private HashSet<Integer> rootAgentSet;
+    private String sumRequestConvId = "average-request";
+    private float guessedNumber;
+
 
     @Override
     protected void setup() {
         Object[] args = getArguments();
         linkedAgents = (Integer[])args.clone();
+        //Collection<Integer> linkedAgentsCollection = Arrays.asList(linkedAgents);
+
+        rootAgentSet = new HashSet<>();
 
         int agentId = Integer.parseInt(getAID().getLocalName());
         Random rand = new Random();
-        number = rand.nextFloat();
-        System.out.println("Agent #" + agentId + " is ready; number guessed: " + number);
+        guessedNumber = rand.nextInt(100);
+        System.out.println("Agent #" + agentId + " is ready; number guessed: " + guessedNumber);
 
-        addBehaviour(new WaitForNumberRequest());
-        addBehaviour(new SendNumberRequest());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (agentId == 0) {
+            addBehaviour(new RequestNumAver());
+        }
+        if (agentId != 0) {
+            addBehaviour(new NumberRequestListener(this, 100));
+        }
     }
+
 
     @Override
     protected void takeDown() {
@@ -35,40 +54,249 @@ public class NumberAgent extends Agent {
         System.out.println("Agent #" + agentId + " is terminating");
     }
 
-    private class SendNumberRequest extends OneShotBehaviour{
-        /*
-        SendNumberRequest(NumberAgent agent, Integer tick) {
-            super(agent, tick);
-        }
-         */
+
+    private class RequestNumAver extends Behaviour {
+        private float globalSum = guessedNumber;
+        private int globalAgentCount = 1;
+        private int propagateCount = 0;
+        private int informCount = 0;
+        private int refuseCount = 0;
+        private String selfIdString;
+        private MessageTemplate mt;
+        private int step = 0;
 
         @Override
         public void action() {
-            int agentId = Integer.parseInt(getAID().getLocalName());
-            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-            msg.setContent(requestMessage);
-            for (Integer linkedAgent : linkedAgents) {
-                msg.addReceiver(new AID(Integer.toString(linkedAgent), AID.ISLOCALNAME));
-                send(msg);
+            switch (step) {
+                case 0:
+                    selfIdString = getAID().getLocalName();
+                    rootAgentSet.add(Integer.parseInt(selfIdString));
+                    ACLMessage sumRequestMsg = new ACLMessage(ACLMessage.REQUEST);
+                    sumRequestMsg.setContent(selfIdString);
+                    sumRequestMsg.setConversationId(sumRequestConvId);
+                    sumRequestMsg.setReplyWith("root-request" + System.currentTimeMillis());
+                    for (Integer linkedAgent : linkedAgents) {
+                        System.out.println("Root Agent #" +
+                                selfIdString +
+                                " sent a request message to its linked Agent #" + linkedAgent);
+                        sumRequestMsg.addReceiver(new AID(Integer.toString(linkedAgent), AID.ISLOCALNAME));
+                    }
+                    myAgent.send(sumRequestMsg);
+                    for (jade.util.leap.Iterator it = sumRequestMsg.getAllReceiver(); it.hasNext(); ) {
+                        System.out.println(it.next());
+                    }
+                    mt = MessageTemplate.and(MessageTemplate.MatchInReplyTo(sumRequestMsg.getReplyWith()),
+                            MessageTemplate.and(MessageTemplate.MatchConversationId(sumRequestConvId),
+                            MessageTemplate.not(MessageTemplate.MatchPerformative(ACLMessage.REQUEST))));
+                    step = 1;
+                    break;
+
+                case 1:
+                    //System.out.println("Root is on STEP 1");
+                    ACLMessage replyMsg = myAgent.receive(mt);
+                    if (replyMsg != null) {
+                        if (replyMsg.getPerformative() == ACLMessage.PROPAGATE) {
+                            float prevSum = Float.parseFloat(replyMsg.getContent());
+                            globalSum += prevSum;
+                            propagateCount++;
+                        }
+                        else if (replyMsg.getPerformative() == ACLMessage.INFORM) {
+                            int prevCount = Integer.parseInt(replyMsg.getContent());
+                            globalAgentCount += prevCount;
+                            informCount++;
+                        }
+                        else if (replyMsg.getPerformative() == ACLMessage.REFUSE) {
+                            refuseCount++;
+                        }
+                        int totalReplyCount = refuseCount + (propagateCount + informCount) / 2;
+                        if (totalReplyCount >= linkedAgents.length) {
+                            System.out.println("Refuses: " + refuseCount);
+                            System.out.println("Propagates: " + propagateCount);
+                            System.out.println("Informs: " + informCount);
+                            step = 2;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    break;
+
+                case 2:
+                    float averageValue = globalSum / globalAgentCount;
+                    System.out.println("Global agent count: " + globalAgentCount);
+                    System.out.println("Root Agent #" + selfIdString + " displaying average value: " + averageValue);
+                    step = 3;
+                    break;
             }
+        }
+
+        @Override
+        public boolean done() {
+            return step == 3;
         }
     }
 
-    private class WaitForNumberRequest extends CyclicBehaviour {
+
+    private class NumberRequestListener extends TickerBehaviour {
+        NumberRequestListener(Agent agent, Integer tickLen) {
+            super(agent, tickLen);
+        }
+
+        private float localSum = guessedNumber;
+        private int localAgentCount = 1;
+        private int propagateCount = 0;
+        private int informCount = 0;
+        private int refuseCount = 0;
+        private AID prevRequester;
+        private ACLMessage incomingMessage;
+        private String rootAgent;
+        private Integer rootAgentNumber;
+        private MessageTemplate mt;
+        private int step = 0;
+
         @Override
-        public void action() {
-            ACLMessage msg = receive();
-            if (msg != null) {
-                if (msg.getContent().equals(requestMessage)) {
-                    int selfId = Integer.parseInt(getAID().getLocalName());
-                    int senderId = Integer.parseInt(msg.getSender().getLocalName());
-                    System.out.println("WAITING SERVICE: Agent #" + senderId +
-                            " requested the number from Agent #" + selfId +
-                            ". The number = " + number);
-                }
-            } else {
-                block();
+        protected void onTick() {
+            String selfIdString = getAID().getLocalName();
+            switch (step) {
+                case 0:
+                    //System.out.println("Agent #" + selfIdString + " is on STEP 0");
+                    mt = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+                            MessageTemplate.MatchConversationId(sumRequestConvId));
+                    incomingMessage = myAgent.receive(mt);
+                    if (incomingMessage != null) {
+                        prevRequester = incomingMessage.getSender();
+                        System.out.println("A #" + selfIdString + " GOT a request FROM A #" + prevRequester.getLocalName());
+                        rootAgent = incomingMessage.getContent();
+                        rootAgentNumber = Integer.parseInt(rootAgent);
+                        if (rootAgentSet.contains(rootAgentNumber)) {
+                            //System.out.println("REFUSE: " + incomingMessage.getPerformative());
+                            ACLMessage reply = incomingMessage.createReply();
+                            reply.setPerformative(ACLMessage.REFUSE);
+                            reply.setConversationId(sumRequestConvId);
+                            myAgent.send(reply);
+                        }
+                        else {
+                            //System.out.println("CONFIRM");
+                            rootAgentSet.add(rootAgentNumber);
+                            step = 1;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    break;
+
+                case 1:
+                    ACLMessage localRequestMsg = new ACLMessage(ACLMessage.REQUEST);
+                    localRequestMsg.setContent(rootAgent);
+                    localRequestMsg.setConversationId(sumRequestConvId);
+                    //localRequestMsg.setReplyWith("local-request" + System.currentTimeMillis());
+
+                    boolean sendPerformed = false;
+                    for (Integer linkedAgent : linkedAgents) {
+                        if (!linkedAgent.equals(Integer.parseInt(prevRequester.getLocalName()))) {
+                            System.out.println("A #" + selfIdString + " SENT a request TO A #" + linkedAgent);
+                            localRequestMsg.addReceiver(new AID(Integer.toString(linkedAgent), AID.ISLOCALNAME));
+                            sendPerformed = true;
+                        }
+                    }
+
+                    if (sendPerformed) {
+                        myAgent.send(localRequestMsg);
+                        //mt = MessageTemplate.and(MessageTemplate.MatchInReplyTo(localRequestMsg.getReplyWith()),
+                        //        MessageTemplate.MatchConversationId(sumRequestConvId));
+                        mt = MessageTemplate.MatchConversationId(sumRequestConvId);
+                        step = 2;
+                    }
+                    else {
+                        step = 3;
+                    }
+                    break;
+
+                case 2:
+                    ACLMessage replyMsg = myAgent.receive(mt);
+                    if (replyMsg != null) {
+                        System.out.println("Agent #" +
+                                selfIdString +
+                                " received performative " +
+                                replyMsg.getPerformative() +
+                                " from A #" +
+                                replyMsg.getSender().getLocalName());
+                        if (replyMsg.getPerformative() == ACLMessage.PROPAGATE) {
+                            float prevSum = Float.parseFloat(replyMsg.getContent());
+                            localSum += prevSum;
+                            propagateCount++;
+                        }
+                        else if (replyMsg.getPerformative() == ACLMessage.INFORM) {
+                            int prevCount = Integer.parseInt(replyMsg.getContent());
+                            localAgentCount += prevCount;
+                            informCount++;
+                        }
+                        else if (replyMsg.getPerformative() == ACLMessage.REFUSE) {
+                            refuseCount++;
+                        }
+                        else if (replyMsg.getPerformative() == ACLMessage.REQUEST &&
+                                !replyMsg.getSender().getLocalName().equals(rootAgent)) {
+                            ACLMessage refuseReply = replyMsg.createReply();
+                            refuseReply.setPerformative(ACLMessage.REFUSE);
+                            refuseReply.setConversationId(sumRequestConvId);
+                            myAgent.send(refuseReply);
+                            System.out.println("A #" + selfIdString +
+                                    " refused to A #" +
+                                    replyMsg.getSender().getLocalName() +
+                                    " from STEP 2");
+                        }
+                        int totalReplyCount = refuseCount + (propagateCount + informCount) / 2;
+                        //System.out.println("A #" + selfIdString + " totalReplies: " + totalReplyCount);
+                        if (totalReplyCount >= linkedAgents.length-1) {
+                            step = 3;
+                            propagateCount = 0;
+                            informCount = 0;
+                            refuseCount = 0;
+                        }
+                    }
+                    else {
+                        block();
+                    }
+                    break;
+
+                case 3:
+                    ACLMessage localSumMsg = incomingMessage.createReply();
+                    ACLMessage localAgentCountMsg = incomingMessage.createReply();
+
+                    localSumMsg.setPerformative(ACLMessage.PROPAGATE);
+                    localAgentCountMsg.setPerformative(ACLMessage.INFORM);
+
+                    localSumMsg.setConversationId(sumRequestConvId);
+                    localAgentCountMsg.setConversationId(sumRequestConvId);
+
+                    localSumMsg.setContent(String.valueOf(localSum));
+                    localAgentCountMsg.setContent(String.valueOf(localAgentCount));
+
+                    System.out.println("A #" + selfIdString + " sent PROPAGATE and INFORM to A #" + prevRequester.getLocalName());
+                    myAgent.send(localSumMsg);
+                    myAgent.send(localAgentCountMsg);
+                    step = 0;
+                    break;
             }
         }
+
+        /*
+        @Override
+        public boolean done() {
+            return (step == 4);
+        }
+        */
+    }
+
+
+    // Misc
+    private boolean isNumeric(String strNum) {
+        try {
+            double d = Float.parseFloat(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
     }
 }
